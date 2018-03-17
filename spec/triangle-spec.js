cf115cf2b8ead7aa0f9acc87f73454750030c259
russell hofvendahl <rhofvendahl@gmail.com>
2018-03-17 01:43:39 -0700

    add mockup copy, bootstrap not working

---
+++ spec/triangle-spec.js
@@ -0,0 +1,13 @@
+// import { Triangle } from './../src/triangle.js';
+//
+// describe('Triangle', function() {
+//   var reusableTriangle;
+//
+//   beforeEach(function() {
+//     reusableTriangle = new Triangle(5, 5, 5);
+//   });
+//
+//   it('should show how beforeEach() works', function() {
+//     console.log(reusableTriangle);
+//   });
+// });
